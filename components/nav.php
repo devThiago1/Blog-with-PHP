3407c597bea2c996a5069f184cac907f1be13485
Thiago Calmon <thiago.calmon4@gmail.com>
2022-12-04 18:17:53 -0300

    atualizações

--- components/nav.php
+++ components/nav.php
@@ -8,7 +8,7 @@
     ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <div class="container-logo" style="margin-right: 25%;">
+            <div class="container-logo" style="margin-right: 38%;">
                 <img src="images/logo.png" style="max-height: 100px; margin-right: 4%;">
                 <a class="nav-link  fs-3 text" id="name" aria-current="page" href="#" style="font-family: 'Share Tech Mono', monospace ;">Newsletter4devs</a>
             </div>
@@ -20,18 +20,13 @@
                     <li class="nav-item fs-5">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
-                    <li class="nav-item fs-5">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
                     <li class="nav-item dropdown fs-5">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
+                        Categorias
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            <li><a class="dropdown-item" href="category.php?cat=geral">Geral</a></li>
+                            <li><a class="dropdown-item" href="category.php?cat=AI">AI</a></li>
                         </ul>
                     </li>
                 </ul>
